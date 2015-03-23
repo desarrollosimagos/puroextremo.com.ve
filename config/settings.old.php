11b0a893064df72912693943f0e0735691872611
Desarrollos Imagos <desarrollosimago@gmail.com>
2015-03-23 15:11:52 -0700

    History.php :wq
    :wq
    :w

--- config/settings.old.php
+++ config/settings.old.php
@@ -1,18 +1,16 @@
 <?php
 define('_DB_SERVER_', 'localhost');
-define('_DB_NAME_', 'tienda-final');
+define('_DB_NAME_', 'labs_puroextremo');
 define('_DB_USER_', 'puro');
 define('_DB_PASSWD_', '8$E41chgQ6Ev');
 define('_DB_PREFIX_', 'ps_');
 define('_MYSQL_ENGINE_', 'InnoDB');
 define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
 define('_PS_CACHE_ENABLED_', '0');
-define('_MEDIA_SERVER_1_', '');
-define('_MEDIA_SERVER_2_', '');
-define('_MEDIA_SERVER_3_', '');
 define('_COOKIE_KEY_', '58T6Jx4735RVG5kOtkjq6tSrFrwKBpgI1O0pABBs52Q9fMXXmYm9O2Fy');
 define('_COOKIE_IV_', 'ak0RMwup');
 define('_PS_CREATION_DATE_', '2013-05-12');
-define('_PS_VERSION_', '1.5.6.1');
+define('_PS_VERSION_', '1.6.0.13');
 define('_RIJNDAEL_KEY_', 'TRa7TcKdnBaWTztsa89gPVLm5FeCyFue');
 define('_RIJNDAEL_IV_', 'i/SYCifjZtjf9bRA3fNsxg==');
+define('_PS_DIRECTORY_', '/../../');
